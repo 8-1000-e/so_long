6e89e68d8bf74a1bccede56ea9c538c746035b10
Emile Dubois--Darthenucq <edubois-@3C2.42angouleme.fr>
2024-11-22 04:15:21 +0100

    projet finis fix norminette and map parse

--- srcs/parse_map_check_data/parse_map.c
+++ srcs/parse_map_check_data/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: edubois- <edubois-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 14:59:01 by edubois-          #+#    #+#             */
-/*   Updated: 2024/11/21 18:15:33 by edubois-         ###   ########.fr       */
+/*   Updated: 2024/11/22 04:12:05 by edubois-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@ static int	parse_body_line(char *line)
 {
 	if (!line)
 		return (0);
-	while (*line && ft_strchr("01CEP\n", *line))
-		line++;
 	if (line[0] != '1' || line[ft_strlen(line) - 2] != '1')
 		return (0);
+	while (*line && ft_strchr("01CEP\n", *line))
+		line++;
 	if (!*line)
 		return (1);
 	return (0);
@@ -38,15 +38,14 @@ static int	parse_body_line(char *line)
 
 int	get_parsed_map(char **tab, t_data *data)
 {
-	int	nb_lines;
-
-	nb_lines = data->max_y + 1;
-	if (!parse_opposed_line(tab[0]) || !parse_opposed_line(tab[data->max_x - 1]))
+	if (!parse_opposed_line(tab[0])
+		|| !parse_opposed_line(tab[data->max_x - 1]))
 		return (0);
-	tab += 1;
-	nb_lines -= 2;
-	while (nb_lines-- && parse_body_line(*tab))
+	tab++;
+	while (*tab && parse_body_line(*tab))
 		tab++;
+	if (*tab)
+		return (0);
 	if (data->max_y * 48 > 1920 || data->max_x * 48 > 1080)
 		return (0);
 	if (!check_format(data->map) || !find_exit(data) || !find_start(data))
